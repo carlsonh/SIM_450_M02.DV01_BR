fdedda7e6569eb7ef112ea2c3753fc04d4e34104
a <a>
2020-09-13 21:21:21 -0700

    Init

---
+++ BR/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviourPun
+{
+
+    [Header("Stats")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Components")]
+    public Rigidbody rb;
+
+
+    public int id;
+    public Player photonPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryJump();
+        }
+    }
+
+    void Move()
+    {
+        //get input axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //calc a dir rela to facing dir
+        Vector3 dir = (transform.forward * z + transform.right * x) * moveSpeed;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir;
+    }
+
+    void TryJump()
+    {
+        //Downwards ray
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        //raycast to check if can jump
+        if (Physics.Raycast(ray, 1.5f))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+
+    [PunRPC]
+    public void Initialize(Player player)
+    {
+        id = player.ActorNumber;
+        photonPlayer = player;
+        GameManager.instance.players[id - 1] = this;
+
+        // is this not our local player?
+        if (!photonView.IsMine)
+        {
+            GetComponentInChildren<Camera>().gameObject.SetActive(false);
+            rb.isKinematic = true;
+        }
+    }
+}
